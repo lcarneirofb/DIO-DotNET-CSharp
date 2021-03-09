a56c1a9f6d22a45d373c870595f4dfa3056cea03
Lucas Carneiro <lcarneirofb@gmail.com>
2021-03-09 01:02:08 -0300

    Segunda Aula .NET + C#, implementação de Pilha em exemplo

---
+++ DIO-Localiza-DotNet-Aula2/ExercicioPilha/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using ExercicioPilha.FolderExample;
+
+namespace ExercicioPilha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pilha_teste = new Pilha();
+            pilha_teste.Empilha(1);
+            pilha_teste.Empilha(10);
+            pilha_teste.Empilha(100);
+
+            Console.WriteLine("Desempilhando o elemento: "+ pilha_teste.Desempilha());
+            Console.WriteLine("Desempilhando o elemento: "+ pilha_teste.Desempilha());
+            Console.WriteLine("Desempilhando o elemento: "+ pilha_teste.Desempilha());
+
+            //Teste Exception Throw
+            Console.WriteLine("Desempilhando o elemento: "+ pilha_teste.Desempilha());
+        }
+    }
+}
